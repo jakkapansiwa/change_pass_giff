2874d276b84863dd577467cf81a4264164be5202
jakkapan siwasaranond <94754073+jakkapansiwa@users.noreply.github.com>
2022-08-25 16:26:20 +0700

    Update checkchange_pass.php

--- checkchange_pass.php
+++ checkchange_pass.php
@@ -3,11 +3,7 @@
 ini_set('memory_limit', '-1');
 set_time_limit(0);
 
-
-mannual
-mannual
-testtest edit
-
+yyyyyyyyyyyyyyyyy
 
 // require_once 'php/db/db.php';
 // require_once("php/db/function_db_v2.php");
