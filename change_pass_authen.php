abd4ee849092cd8a2260adbf18fce7aea05605a6
jakkapan siwasaranond <94754073+jakkapansiwa@users.noreply.github.com>
2022-09-02 23:23:23 +0700

    test 2

--- change_pass_authen.php
+++ change_pass_authen.php
@@ -1,39 +1,6 @@
 
-</script>
+KKK
 
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="jqauthen.js "></script>
+MMM
 
-</body>
-</html>
-
-</script>
-aaaaa
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="jqauthen.js "></script>
-
-</body>
-</html>
-
-</script>
-
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="jqauthen.js "></script>
-
-</body>
-</html>
-</script>
-
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="jqauthen.js "></script>
-
-</body>
-</html>
-</script>
-
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="jqauthen.js "></script>
-
-</body>
-</html>
-
+AAA
